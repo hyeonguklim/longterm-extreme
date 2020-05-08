705c1e53d81e14833cf23e8364e84379d86d3801
Harry <hyeonguklim@gmail.com>
2020-05-07 21:23:07 -0500

    updated plot_exprob

--- plot_exprob.m
+++ plot_exprob.m
@@ -1,17 +1,45 @@
 clear;clc;close all;
 
-MCS = load('mat_files\MCS_100000.mat');
-IS = load('mat_files\IS_100000.mat');
+% load mat files
+mat1 = load('mat_files\MCS_100000.mat');
+mat2 = load('mat_files\IS_100000.mat');
+
+Z1 = mat1.Z; % MCS
+Z2 = mat2.Z; % IS
+Weight = mat2.weight; % weight for IS
 
 figure();hold on;
 for ii = 1:10
-    Z1 = MCS.Z( (ii-1)*10000+1:ii*10000 );
-    Z2 = IS.Z( (ii-1)*10000+1:ii*10000 );
-    [cdf1,z1] = ecdf(Z1);
-    [cdf2,z2] = ecdf(Z2);
-    plot(z1,1-cdf1,'r-','linewidth',1);
-    plot(z2,1-cdf2,'b--','linewidth',1);
+    
+    % divide into a set
+    z1 = Z1( (ii-1)*10000+1:ii*10000 );
+    z2 = Z2( (ii-1)*10000+1:ii*10000 );
+    weight = Weight( (ii-1)*10000+1:ii*10000 );
+    
+    % calculate exceedance probabilities for MCS
+    z1uniq = unique(z1);
+    len = length(z1);
+    for jj = 1:len
+        idx = z1 > z1uniq(jj);
+        exprob1(jj) = sum(idx)/len;
+        clear idx
+    end
+    
+    % calculate exceedance probabilities for IS
+    z2uniq = unique(z2);
+    len = length(z2);
+    for jj = 1:len
+        idx = z2 > z2uniq(jj);
+        exprob2(jj) = sum(weight(idx))/len;
+        clear idx
+    end
+    
+    % plot
+    plot(z1uniq,exprob1,'r-','linewidth',2);
+    plot(z2uniq,exprob2,'b--','linewidth',2);
 end
+leg = legend('MCS','MCS-IS');legend boxoff
+set(leg,'interpreter','latex')
 set(gca,'yscale','log',...
     'box','on',...
     'yscale','log',...
